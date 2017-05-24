5ff470870dddb858e1b46c055d7974b3b5845b5f
YiLiu6240 <yi.liu6240@gmail.com>
2017-05-24 22:06:16 +0100

    add feature `yxl-dash`

---
+++ yxl-dash.el
@@ -0,0 +1,30 @@
+(require 'counsel-dash)
+
+;; A rewrite of the stock spacemacs layer to fit my needs
+
+(defvar yxl-dash-docset-path "~/.docsets"
+  "Path containing dash docsets.")
+
+(defvar yxl-dash-browser-func 'browse-url
+  "Func to browse dash docsets.")
+
+(defun yxl-dash-activate-package-docsets (path)
+  "Add dash docsets from specified PATH."
+  (setq helm-dash-docsets-path (expand-file-name path))
+  (setq helm-dash-common-docsets (helm-dash-installed-docsets))
+  (message (format "activated %d docsets from: %s"
+                   (length helm-dash-common-docsets) path)))
+
+(defun yxl-dash-search-docset ()
+  (interactive)
+  (let* ((counsel-dash-common-docsets
+          (list (ivy-read "which docset to use: "
+                          (helm-dash-installed-docsets)))))
+    (counsel-dash)))
+
+(defun yxl-dash-search-docset-external-browser ()
+  (interactive)
+  (let ((counsel-dash-browser-func 'browse-url-generic))
+    (yxl-dash-search-docset)))
+
+(provide 'yxl-dash)
