f17c84c562a139369b74377f2e4311f7a8910b9b
YiLiu6240 <yi.liu6240@gmail.com>
2018-03-02 17:50:25 +0000

    Add search history to yxl-dash

--- yxl-dash.el
+++ yxl-dash.el
@@ -8,6 +8,9 @@
 (defvar yxl-dash-browser-func 'browse-url
   "Func to browse dash docsets.")
 
+(defvar yxl-dash-search-history nil
+  "Search history for `yxl-dash-search-docset'")
+
 (defun yxl-dash-activate-package-docsets (path)
   "Add dash docsets from specified PATH."
   (setq helm-dash-docsets-path (expand-file-name path))
@@ -19,7 +22,11 @@
   (interactive)
   (let* ((counsel-dash-common-docsets
           (list (ivy-read "which docset to use: "
-                          (helm-dash-installed-docsets)))))
+                          (helm-dash-installed-docsets)
+                          :history 'yxl-dash-search-history
+                          :preselect (if (listp yxl-dash-search-history)
+                                         (car yxl-dash-search-history)
+                                       yxl-dash-search-history)))))
     (counsel-dash)))
 
 (defun yxl-dash-search-docset-external-browser ()
